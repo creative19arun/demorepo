8886ba4b84d3bdf5c2c2bb2a97a0888378d43f58
creative19arun <creative19arun@gmail.com>
2026-03-29 05:49:36 -0700

    uploading code to github

---
+++ first prgm.py
@@ -0,0 +1,24 @@
+print("hello world")
+print("Arun is my name.","My age is 35")
+b=3
+c=6
+print(b+c)
+
+"""
+print(not True)
+print(not (b==c))
+
+print(35)
+print(55+35)
+name="Arun"
+age=35
+price=99.099
+print(name)
+print("Age=",age)
+print(type(price))
+print(type(name))
+print(type(age))
+name='moti'
+old=True
+print(type(old))
+"""
